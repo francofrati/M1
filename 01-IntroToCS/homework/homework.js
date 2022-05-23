82f16ba9cc622aed57b23b79d257c332cec9d53d
francofrati <francofraticelliff@gmail.com>
2022-05-23 16:30:47 -0300

    HW Intro BinDec

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,13 +2,37 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var binStr = num.toString().split('').reverse();
+  var numDec = 0;
+  for(let i=0;i<binStr.length;i++){
+    binStr[i] = binStr[i]*(2**i)
+    binStr[i] = Math.pow(binStr[i],1)
+  }
+  for(let j=0;j<binStr.length;j++){
+    numDec = numDec + binStr[j]
+  }
+  return numDec
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var numCamb = num
+  var arrBin = [];
 
-}
+  for(let i = 0 ; i >-1 ; i++){
+    if(numCamb<1 && numCamb>0)
+    { 
+      var zarrBin = arrBin.reverse().join('')
+      return zarrBin
+    }
+    var zz = Math.floor(numCamb%2)
+    arrBin.push(zz)
+    numCamb = numCamb/2
+    
+  }
+   
+
+}  
 
 
 module.exports = {
